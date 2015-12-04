6251b94dfdd45fb99a179626e85515b0eb14c512
fortkle <fortkle@gmail.com>
2015-12-04 19:41:04 +0900

    Initial Commit

---
+++ js/slack-jpn.js
@@ -0,0 +1,15 @@
+$(function(){
+
+  // メインページビュー
+  $('#starred_section_header').text('★お気に入り');
+  $('#channels_header').find('span').text('チャンネル');
+
+  // サイドバーメニュー
+  $(document).on('click', '#team_menu', function() {
+    $(document).find('#member_prefs_item a').text('設定');
+    $(document).find('#member_account_item a').text('プロフィールを見る');
+    $(document).find('#team_help a').text('ヘルプ＆フィードバック');
+    $(document).find('#team_apps a').text('Slackアプリをダウンロード');
+  });
+
+});
