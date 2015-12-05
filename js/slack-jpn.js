d58a5994b043fe062190fcf8ab7dc74855c68a49
fortkle <fortkle@gmail.com>
2015-12-05 23:59:19 +0900

    comment out

--- js/slack-jpn.js
+++ js/slack-jpn.js
@@ -5,11 +5,11 @@ $(function(){
   $('#channels_header').find('span').text('チャンネル');
 
   // サイドバーメニュー
-  $(document).on('click', '#team_menu', function() {
-    $(document).find('#member_prefs_item a').text('設定');
-    $(document).find('#member_account_item a').text('プロフィールを見る');
-    $(document).find('#team_help a').text('ヘルプ＆フィードバック');
-    $(document).find('#team_apps a').text('Slackアプリをダウンロード');
-  });
+  //$(document).on('click', '#team_menu', function() {
+  //  $(document).find('#member_prefs_item a').text('設定');
+  //  $(document).find('#member_account_item a').text('プロフィールを見る');
+  //  $(document).find('#team_help a').text('ヘルプ＆フィードバック');
+  //  $(document).find('#team_apps a').text('Slackアプリをダウンロード');
+  //});
 
 });
